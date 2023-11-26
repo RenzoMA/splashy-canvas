1512ba87ec0a9dbd817f833eadea9597e0bfce00
Renzo Maldonado <renzoma89@gmail.com>
2023-11-26 18:19:20 -0500

    fix new unicorn

--- src/app/instructions/components/landing-instructions/landing-instructions.component.ts
+++ src/app/instructions/components/landing-instructions/landing-instructions.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-landing-instructions',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './landing-instructions.component.html',
-  styleUrl: './landing-instructions.component.scss'
+	selector: 'app-landing-instructions',
+	standalone: true,
+	imports: [CommonModule, RouterLink],
+	templateUrl: './landing-instructions.component.html',
+	styleUrl: './landing-instructions.component.scss',
 })
-export class LandingInstructionsComponent {
-
-}
+export class LandingInstructionsComponent {}
