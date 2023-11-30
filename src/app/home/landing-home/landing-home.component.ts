21985f1860e3d733fc44eeb0577a71984f8bf855
Renzo Maldonado <renzoma89@gmail.com>
2023-11-30 13:19:22 -0500

    test

--- src/app/home/landing-home/landing-home.component.ts
+++ src/app/home/landing-home/landing-home.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-landing-home',
 	standalone: true,
-	imports: [CommonModule, RouterLink],
+	imports: [CommonModule, RouterLink, NgOptimizedImage],
 	templateUrl: './landing-home.component.html',
 	styleUrl: './landing-home.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
