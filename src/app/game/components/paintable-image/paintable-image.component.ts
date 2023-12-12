ef5e1b078bbd0daf4044763a64e58f6691bb5fa4
Renzo Maldonado <renzoma89@gmail.com>
2023-12-11 23:12:46 -0500

    test afterNextRender

--- src/app/game/components/paintable-image/paintable-image.component.ts
+++ src/app/game/components/paintable-image/paintable-image.component.ts
@@ -1,4 +1,13 @@
-import { AfterViewInit, Component, ElementRef, Input, Renderer2, inject } from '@angular/core';
+import {
+	AfterRenderPhase,
+	AfterViewInit,
+	Component,
+	ElementRef,
+	Input,
+	Renderer2,
+	afterNextRender,
+	inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeHtmlPipe } from '../../../util/safe-html.pipe';
 
@@ -15,19 +24,23 @@ export class PaintableImageComponent implements AfterViewInit {
 	element = inject(ElementRef);
 	renderer = inject(Renderer2);
 
-	ngAfterViewInit(): void {
-		const svgElement = this.element.nativeElement.querySelector('svg');
-		if (svgElement) {
-      this.renderer.setStyle(svgElement, 'max-width','100%');
-      this.renderer.setStyle(svgElement, 'height','auto');
-			const paths = svgElement.querySelectorAll('path');
-			paths.forEach((path: any) => {
-				this.renderer.removeAttribute(path, 'style');
-				this.renderer.setStyle(path, 'fill', 'transparent');
-				this.renderer.listen(path, 'click', () => {
-					this.renderer.setStyle(path, 'fill', this.currentColor);
+	constructor() {
+		afterNextRender(() => {
+			const svgElement = this.element.nativeElement.querySelector('svg');
+			if (svgElement) {
+				this.renderer.setStyle(svgElement, 'max-width', '100%');
+				this.renderer.setStyle(svgElement, 'height', 'auto');
+				const paths = svgElement.querySelectorAll('path');
+				paths.forEach((path: any) => {
+					this.renderer.removeAttribute(path, 'style');
+					this.renderer.setStyle(path, 'fill', 'transparent');
+					this.renderer.listen(path, 'click', () => {
+						this.renderer.setStyle(path, 'fill', this.currentColor);
+					});
 				});
-			});
-		}
+			}
+		});
 	}
+
+	ngAfterViewInit(): void {}
 }
